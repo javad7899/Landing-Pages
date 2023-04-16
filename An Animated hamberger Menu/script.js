c503851edb04b8c08c1b8188c5dca0cb6097ca53
javad <javadhashemzadeh78@gmail.com>
2023-04-17 03:09:08 +0330

    Added All Files

---
+++ An Animated hamberger Menu/script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.getElementById("hamberger-menu");
+const navBar = document.getElementById("navbar");
+menuIcon.addEventListener("click", () =>{
+navBar.classList.toggle("change");
+});
